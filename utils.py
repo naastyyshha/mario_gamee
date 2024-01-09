27deccb3e6577b46794b38c06b470e75980a7363
naastyyshha <anastasiakorotkevich@icloud.com>
2024-01-09 20:26:05 +0700

    Add file

--- utils.py
+++ utils.py
@@ -7,12 +7,10 @@ FPS = 60
 
 
 class Camera:
-    # зададим начальный сдвиг камеры
     def __init__(self):
         self.dx = 0
         self.dy = 0
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
         obj.rect.y += self.dy
